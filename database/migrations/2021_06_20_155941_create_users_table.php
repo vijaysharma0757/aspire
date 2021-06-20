c326fd794d447b00185f41ba4c7bf5c016ecc3ce
vijay sharma <vijaysharma0757@gmail.com>
2021-06-20 21:43:32 +0530

    initial commit

---
+++ database/migrations/2021_06_20_155941_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email')->unique()->notNullable();
+            $table->string('password');
+            $table->timestamps();
+        });
+        DB::table('users')->insert([
+            'id' => 1,
+            'name' => 'vijay',
+            'email' => 'vijaysharma0757@gmail.com',
+            'password' => app('hash')->make('jack1234'),
+            'created_at' => date('Y-m-d H:i:s', time()),
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
